fe7c0bcd3938098b21ee08558f0db487df001378
Matthew Cross <matthewkacross@gmail.com>
2025-03-17 02:15:15 -0500

    Completed Exercise 7

--- app/views.py
+++ app/views.py
@@ -76,6 +76,13 @@ def files():
     images = get_uploaded_images() 
     return render_template("files.html", images=images)
 
+@app.route('/logout')
+@login_required  
+def logout():
+    logout_user()  
+    flash("You have been logged out.", "success")  
+    return redirect(url_for("home")) 
+
 # user_loader callback. This callback is used to reload the user object from
 # the user ID stored in the session
 @login_manager.user_loader
