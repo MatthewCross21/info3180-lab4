08d662a3e585464c194caa47a3002f6708c1085b
Matthew Cross <matthewkacross@gmail.com>
2025-03-17 01:53:08 -0500

    Completed Exercise 5

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,17 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField
+from wtforms import StringField, PasswordField, SubmitField,FileField
 from wtforms.validators import InputRequired
+from flask_wtf.file import FileAllowed
 
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[InputRequired()])
     password = PasswordField('Password', validators=[InputRequired()])
+    submit = SubmitField('Login')
+
+class UploadForm(FlaskForm):
+    file = FileField("Upload File", validators=[
+        InputRequired(),
+        FileAllowed(['jpg', 'png'], "Only JPG and PNG images")  
+    ])
+    submit = SubmitField("Upload")
--- app/views.py
+++ app/views.py
@@ -6,7 +6,7 @@ from werkzeug.utils import secure_filename
 from app.models import UserProfile
 from app.forms import LoginForm
 from werkzeug.security import check_password_hash
-
+from app.forms import UploadForm
 
 
 ###
@@ -26,17 +26,20 @@ def about():
 
 
 @app.route('/upload', methods=['POST', 'GET'])
+@login_required
 def upload():
-    # Instantiate your form class
+    form = UploadForm()
 
     # Validate file upload on submit
     if form.validate_on_submit():
-        # Get file data and save to your uploads folder
+        file = form.file.data
+        filename = secure_filename(file.filename)  
 
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
         flash('File Saved', 'success')
         return redirect(url_for('home')) # Update this to redirect the user to a route that displays all uploaded image files
 
-    return render_template('upload.html')
+    return render_template('upload.html',form=form)
 
 
 @app.route('/login', methods=['POST', 'GET'])
